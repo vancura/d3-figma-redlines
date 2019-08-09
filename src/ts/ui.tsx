92624a1ec6c926a16dfaef59de7d7491c48dcdc5
Václav Vančura <vaclav@vancura.org>
2019-08-09 17:46:22 +0200

    Window helper rollovers

--- src/ts/ui.tsx
+++ src/ts/ui.tsx
@@ -46,6 +46,74 @@ class D3Redlines extends React.Component<{}, {showSelectionError: boolean; lineO
     };
 
 
+    showHelper = (id: string) => {
+        // TODO: Clean up this mess
+
+        switch (id) {
+            case "ghostbox":
+                document.getElementById("helper-ghostbox").classList.add("is-visible");
+                break;
+
+            case "legend-left":
+                document.getElementById("helper-legend-left").classList.add("is-visible");
+                break;
+
+            case "legend-right":
+                document.getElementById("helper-legend-right").classList.add("is-visible");
+                break;
+
+            case "redline-h-top":
+                document.getElementById("helper-redline-h-top").classList.add("is-visible");
+                break;
+
+            case "redline-v-top":
+                document.getElementById("helper-redline-v-top").classList.add("is-visible");
+                break;
+
+            case "redline-v-bot":
+                document.getElementById("helper-redline-v-bot").classList.add("is-visible");
+                break;
+
+            case "redline-h-bot":
+                document.getElementById("helper-redline-h-bot").classList.add("is-visible");
+                break;
+
+            case "redline-h-left":
+                document.getElementById("helper-redline-h-left").classList.add("is-visible");
+                break;
+
+            case "redline-v-left":
+                document.getElementById("helper-redline-v-left").classList.add("is-visible");
+                break;
+
+            case "redline-h-right":
+                document.getElementById("helper-redline-h-right").classList.add("is-visible");
+                break;
+
+            case "redline-v-right":
+                document.getElementById("helper-redline-v-right").classList.add("is-visible");
+                break;
+        }
+    };
+
+
+    hideHelper = () => {
+        // TODO: Iterate this crap
+
+        document.getElementById("helper-ghostbox").classList.remove("is-visible");
+        document.getElementById("helper-legend-left").classList.remove("is-visible");
+        document.getElementById("helper-legend-right").classList.remove("is-visible");
+        document.getElementById("helper-redline-h-top").classList.remove("is-visible");
+        document.getElementById("helper-redline-v-top").classList.remove("is-visible");
+        document.getElementById("helper-redline-v-bot").classList.remove("is-visible");
+        document.getElementById("helper-redline-h-bot").classList.remove("is-visible");
+        document.getElementById("helper-redline-h-left").classList.remove("is-visible");
+        document.getElementById("helper-redline-v-left").classList.remove("is-visible");
+        document.getElementById("helper-redline-h-right").classList.remove("is-visible");
+        document.getElementById("helper-redline-v-right").classList.remove("is-visible");
+    };
+
+
     componentDidMount() {
         require("../../external/figma-ui/scripts.min.js");
         require("../../external/figma-plugin-ds/js/disclosure.js");
@@ -70,12 +138,6 @@ class D3Redlines extends React.Component<{}, {showSelectionError: boolean; lineO
 
     render() {
         return (<div className="l-main">
-            <section
-                onClick={() => this.setState({showSelectionError: false})}
-                className={"visual-bell visual-bell--error " + (this.state.showSelectionError ? "" : "is-hidden")}>
-                <span className="visual-bell__msg">Please select at least one element</span>
-            </section>
-
             <header className="l-window-header">
                 <img src={require("../images/d3-logo.svg")} />
 
@@ -86,36 +148,36 @@ class D3Redlines extends React.Component<{}, {showSelectionError: boolean; lineO
             </header>
 
             <section className="l-selector">
-                <button id="legend-left" className="button button--secondary">Legend</button>
-                <button id="legend-right" className="button button--secondary">Legend</button>
+                <button id="button-legend-left" className="button button--secondary" onMouseOver={() => this.showHelper("legend-left")} onMouseOut={() => this.hideHelper()}>Legend</button>
+                <button id="button-legend-right" className="button button--secondary" onMouseOver={() => this.showHelper("legend-right")} onMouseOut={() => this.hideHelper()}>Legend</button>
 
-                <button id="ghostbox" className="button button--secondary">Ghost Box</button>
+                <button id="button-ghostbox" className="button button--secondary" onMouseOver={() => this.showHelper("ghostbox")} onMouseOut={() => this.hideHelper()}>Ghost Box</button>
 
-                <button id="redline-h-top" className="button button--secondary">H Redline Top</button>
-                <button id="redline-v-top" className="button button--secondary">V Redline Top</button>
-                <button id="redline-v-bot" className="button button--secondary">V Redline Bottom</button>
-                <button id="redline-h-bot" className="button button--secondary">H Redline Bottom</button>
+                <button id="button-redline-h-top" className="button button--secondary" onMouseOver={() => this.showHelper("redline-h-top")} onMouseOut={() => this.hideHelper()}>H Redline Top</button>
+                <button id="button-redline-v-top" className="button button--secondary" onMouseOver={() => this.showHelper("redline-v-top")} onMouseOut={() => this.hideHelper()}>V Redline Top</button>
+                <button id="button-redline-v-bot" className="button button--secondary" onMouseOver={() => this.showHelper("redline-v-bot")} onMouseOut={() => this.hideHelper()}>V Redline Bottom</button>
+                <button id="button-redline-h-bot" className="button button--secondary" onMouseOver={() => this.showHelper("redline-h-bot")} onMouseOut={() => this.hideHelper()}>H Redline Bottom</button>
 
-                <button id="redline-h-left" className="button button--secondary">H Redline Left</button>
-                <button id="redline-v-left" className="button button--secondary">V Redline Left</button>
-                <button id="redline-h-right" className="button button--secondary">H Redline Right</button>
-                <button id="redline-v-right" className="button button--secondary">V Redline Right</button>
+                <button id="button-redline-h-left" className="button button--secondary" onMouseOver={() => this.showHelper("redline-h-left")} onMouseOut={() => this.hideHelper()}>H Redline Left</button>
+                <button id="button-redline-v-left" className="button button--secondary" onMouseOver={() => this.showHelper("redline-v-left")} onMouseOut={() => this.hideHelper()}>V Redline Left</button>
+                <button id="button-redline-h-right" className="button button--secondary" onMouseOver={() => this.showHelper("redline-h-right")} onMouseOut={() => this.hideHelper()}>H Redline Right</button>
+                <button id="button-redline-v-right" className="button button--secondary" onMouseOver={() => this.showHelper("redline-v-right")} onMouseOut={() => this.hideHelper()}>V Redline Right</button>
 
                 <img src={require("../images/selection-bg.svg")} id="selection-bg" className="center" />
-                <img src={require("../images/ghostbox.svg")} id="ghostbox" className="center ghostbox" />
+                <img src={require("../images/ghostbox.svg")} id="helper-ghostbox" className="center is-ghostbox" />
                 <img src={require("../images/selection-fg.svg")} id="selection-fg" className="center" />
 
-                <img src={require("../images/legend-left.svg")} id="legend-left" className="center pad legend" />
-                <img src={require("../images/legend-right.svg")} id="legend-right" className="center pad legend" />
+                <img src={require("../images/legend-left.svg")} id="helper-legend-left" className="center pad is-legend" />
+                <img src={require("../images/legend-right.svg")} id="helper-legend-right" className="center pad is-legend" />
 
-                <img src={require("../images/redline-h-left.svg")} id="redline-h-left" className="center pad redline" />
-                <img src={require("../images/redline-h-right.svg")} id="redline-h-right" className="center pad redline" />
-                <img src={require("../images/redline-h-top.svg")} id="redline-h-top" className="center pad redline" />
-                <img src={require("../images/redline-h-bot.svg")} id="redline-h-bot" className="center pad redline" />
-                <img src={require("../images/redline-v-top.svg")} id="redline-v-top" className="center pad redline" />
-                <img src={require("../images/redline-v-bot.svg")} id="redline-v-bot" className="center pad redline" />
-                <img src={require("../images/redline-v-right.svg")} id="redline-v-right" className="center pad redline" />
-                <img src={require("../images/redline-v-left.svg")} id="redline-v-left" className="center pad redline" />
+                <img src={require("../images/redline-h-left.svg")} id="helper-redline-h-left" className="center pad is-redline" />
+                <img src={require("../images/redline-h-right.svg")} id="helper-redline-h-right" className="center pad is-redline" />
+                <img src={require("../images/redline-h-top.svg")} id="helper-redline-h-top" className="center pad is-redline" />
+                <img src={require("../images/redline-h-bot.svg")} id="helper-redline-h-bot" className="center pad is-redline" />
+                <img src={require("../images/redline-v-top.svg")} id="helper-redline-v-top" className="center pad is-redline" />
+                <img src={require("../images/redline-v-bot.svg")} id="helper-redline-v-bot" className="center pad is-redline" />
+                <img src={require("../images/redline-v-right.svg")} id="helper-redline-v-right" className="center pad is-redline" />
+                <img src={require("../images/redline-v-left.svg")} id="helper-redline-v-left" className="center pad is-redline" />
 
 
 {/*
